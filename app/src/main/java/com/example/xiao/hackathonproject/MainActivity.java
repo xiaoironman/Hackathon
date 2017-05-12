fd27f8d237a18a6457a7acc561d9b6b0858417a8
dufi-david <davidhriadel@gmail.com>
2017-05-12 18:03:01 +0200

    Added println.,again.

--- app/src/main/java/com/example/xiao/hackathonproject/MainActivity.java
+++ app/src/main/java/com/example/xiao/hackathonproject/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Fuck the world!");
+        System.out.println("FUCK GITHUB");
     }
 
     private void xiao()
