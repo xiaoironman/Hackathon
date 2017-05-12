cc3002bd799d5ae8fdc6b240ef13353c365d2922
dufi-david <davidhriadel@gmail.com>
2017-05-12 17:53:27 +0200

    Added new function.

--- app/src/main/java/com/example/xiao/hackathonproject/MainActivity.java
+++ app/src/main/java/com/example/xiao/hackathonproject/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.xiao.hackathonproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        heyXiao();
+    }
+
+    protected void heyXiao() {
+        System.out.println("Hey!");
     }
 }
