96f953515539e9c5c8303918c79b44b47527b411
xiaoironman <xiaoironman@gmail.com>
2017-05-12 22:41:29 +0200

    Added the new google map activity and connected the first button to it

--- app/src/main/java/com/example/xiao/hackathonproject/MainActivity.java
+++ app/src/main/java/com/example/xiao/hackathonproject/MainActivity.java
@@ -8,8 +8,11 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,9 +23,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //The code for customized toolbar;
         Toolbar my_toolbar = (Toolbar) findViewById(R.id.CogeoToolbar);
         setSupportActionBar(my_toolbar);
-        getSupportActionBar().setTitle("COGEO");
+        getSupportActionBar().setTitle("COGEO"); //Change the Toolbar title here!!!
 
         // check if the permission is granted for getting a fine location; if not, ask for one
         int permissionCheck = ContextCompat.checkSelfPermission(this,
@@ -32,7 +36,14 @@ public class MainActivity extends AppCompatActivity {
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     MY_PERMISSIONS_REQUEST_FINE_LOCATION);
         }
-        
+
+    }
+
+    //Click the first button(oclick set to onClick1) to go to the rough location page;
+    public void onClick1(View v)
+    {
+        Intent myIntent = new Intent(this,RoughLocation.class);
+        startActivity(myIntent);
     }
 
     @Override
