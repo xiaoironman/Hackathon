b8701185c526f226956fbdfe5d196542d40c2ef6
xiaoironman <xiaoironman@gmail.com>
2017-05-13 01:07:55 +0200

    reference list activity added

---
+++ app/src/main/java/com/example/xiao/hackathonproject/ReferenceList.java
@@ -0,0 +1,13 @@
+package com.example.xiao.hackathonproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReferenceList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reference_list);
+    }
+}
