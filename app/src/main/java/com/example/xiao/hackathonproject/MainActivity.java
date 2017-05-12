73ff8e6f689c6e19eedc5cf318af6cadf7514315
xiaoironman <xiaoironman@gmail.com>
2017-05-12 17:58:47 +0200

    added Xiao's function

--- app/src/main/java/com/example/xiao/hackathonproject/MainActivity.java
+++ app/src/main/java/com/example/xiao/hackathonproject/MainActivity.java
@@ -11,7 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        xiao();
     }
-
+private void xiao()
+{
+    System.out.println("I'm Xiao!");
+}
 
 }
