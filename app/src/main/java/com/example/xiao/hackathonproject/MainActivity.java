88188318eb306eef78774c06e49c332ce48fb54d
xiaoironman <xiaoironman@gmail.com>
2017-05-12 17:56:47 +0200

    deleted function

--- app/src/main/java/com/example/xiao/hackathonproject/MainActivity.java
+++ app/src/main/java/com/example/xiao/hackathonproject/MainActivity.java
@@ -11,10 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        heyXiao();
     }
 
-    protected void heyXiao() {
-        System.out.println("Hey!");
-    }
+
 }
