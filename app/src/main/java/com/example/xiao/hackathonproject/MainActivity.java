37f3e69703feb6e47d44b6d5c2ee093838c14242
dufi-david <davidhriadel@gmail.com>
2017-05-12 20:28:02 +0200

    Permission check and request implemented.

--- app/src/main/java/com/example/xiao/hackathonproject/MainActivity.java
+++ app/src/main/java/com/example/xiao/hackathonproject/MainActivity.java
@@ -1,15 +1,46 @@
 package com.example.xiao.hackathonproject;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // check if the permission is granted for getting a fine location; if not, ask for one
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionCheck == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_FINE_LOCATION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission granted
+                    Toast.makeText(this, "Permission granted.", Toast.LENGTH_LONG).show();
+                } else {
+                    // permission denied
+                    Toast.makeText(this, "Permission not granted.", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
     }
 }
